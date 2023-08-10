e4cec2848c931a85d517e6cdaa6d4f24eea41857
Moiseev Kirill <kilkuruzzza@mail.ru>
2023-08-10 13:50:15 +0300

    1

---
+++ untitled/src/ModelElements/PoligonalModel.java
@@ -0,0 +1,14 @@
+package ModelElements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PoligonalModel {
+    public List<Poligon> Poligons;
+    public List<Texture> Textures;
+
+    public PoligonalModel(List<Texture> textures) {
+        Textures = textures;
+        Poligons = new ArryList<>();
+    }
+}
