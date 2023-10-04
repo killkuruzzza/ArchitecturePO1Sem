76224bc17cb3a68b4bde67d9f57d9d70a4b25bd6
killkuruzzza <46843343+killkuruzzza@users.noreply.github.com>
2023-10-04 19:12:28 +0300

    Update PoligonalModel.java

--- untitled/src/ModelElements/PoligonalModel.java
+++ untitled/src/ModelElements/PoligonalModel.java
@@ -10,5 +10,6 @@ public class PoligonalModel {
     public PoligonalModel(List<Texture> textures) {
         Textures = textures;
         Poligons = new ArrayList<>();
+        Poligons.add(new Poligon(new Point3D()));
     }
 }
